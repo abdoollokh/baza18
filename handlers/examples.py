f75c0aa57fd7c10155b5d3a011c18f17677a23db
abdoollokh <64684223+abdoollokh@users.noreply.github.com>
2022-10-10 16:28:43 +0500

    Update examples.py

--- handlers/examples.py
+++ handlers/examples.py
@@ -3,6 +3,6 @@ from aiogram import types
 
 @dp.message_handler(text='💡 Bot qanday ishlaydi 💡')
 async def examples(message: types.Message):
-    photo='https://i.imgur.com/hL6h5cf.jpg'
+    photo='https://i.postimg.cc/Bn4mGhYc/ezgif-2-bdedcca650.gif'
     text = '🔎 Bot har qanday oldin yuborilgan yoki tarqab ketilgan rasm va videolar orasidan siz hohlagan odamingizni topib beradi!\n\n1️⃣ Bazada 10.000.000 dan ortiq qachondir yuborilgan 🔞 video va rasmlar bor. Bu hammasi professional dasturchi va hakkerlar tomonidan to`plangan\n\nEng qizig`i ko`plab video va rasmlarni o`z yaqinlari pul evaziga tashlab beriladi!'
     await message.answer_photo(photo, text)
