f11b30092a6c40fd5a188bc611948c6b8b072b3c
abdoollokh <64684223+abdoollokh@users.noreply.github.com>
2022-10-10 15:38:43 +0500

    Update referals.py

--- handlers/referals.py
+++ handlers/referals.py
@@ -7,5 +7,5 @@ async def referals(message: types.Message):
     count = database.get_user_referals_count(message.from_user.id)
     balance = database.get_user(message.from_user.id)['balance']
     username = (await message.bot.me).username
-    text = f'''<b>Ваша реферальная ссылка:</b>\nhttps://t.me/{username}?start={message.from_user.id}\n\n<b>Taklif qilinganlar soni: </b><code>{count}</code>\n<b>Xisob: </b><code>{balance}</code>\n\n<i>Do`stlaringizni taklif qiling va ularning xarididan 50% xisobingizga oling!</i>\n\nReferal xisobdan yechib olish uchun, bu buyruqni bering /pay'''
+    text = f'''<b>Sizninh referal ssilkangiz:</b>\nhttps://t.me/{username}?start={message.from_user.id}\n\n<b>Taklif qilinganlar soni: </b><code>{count}</code>\n<b>Xisob: </b><code>{balance}</code>\n\n<i>Do`stlaringizni taklif qiling va ularning xarididan 50% xisobingizga oling!</i>\n\nReferal xisobdan yechib olish uchun, bu buyruqni bering /pay'''
     await message.answer(text)
